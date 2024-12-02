fe86e675932930c23368dd9278ee8bcd36e3b368
PRout1911 <routpriyanka1911@gmail.com>
2024-12-02 15:08:35 +0530

    learn variable

---
+++ basics_01/variable.js
@@ -0,0 +1,7 @@
+const abc = 1
+let name1 = "abs"
+var mail = "abc@maiil.com" /* Don't use VAR to declare varaibles as js have some issues with block and functional scope*/
+let xyz; // js will name it as undefined
+console.log(name1); // use to print a single output
+console.table([name1, mail]); // use to print multiple output in a tabular form
+xyz = '123' //this is not a good practice
