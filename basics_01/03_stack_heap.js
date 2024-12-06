fe586447c21f1df3c03881ffbb3e2594d05edb49
PRout1911 <routpriyanka1911@gmail.com>
2024-12-06 16:58:40 +0530

    new files add

---
+++ basics_01/03_stack_heap.js
@@ -0,0 +1,16 @@
+//Stack (Primitive): we get the copy of the value 
+let nameOne = "abc"
+let nameTwo = nameOne
+nameTwo = "pqr"
+console.log(nameTwo);
+console.log(nameOne);
+
+// Heap (Non-Primitive): we get the reference of the value
+let myObj = {
+    email: "anc@gmail.com",
+    id: 123
+}
+let myObj1 = myObj
+myObj1.email = "pqr@gmail.com"
+console.log(myObj.email);
+console.log(myObj1.email);
