97af59e6d7d870011633875b76a0671031732b17
PRout1911 <routpriyanka1911@gmail.com>
2024-12-12 17:32:45 +0530

    objects

---
+++ 02_basics/02_objects.js
@@ -0,0 +1,71 @@
+//Singleton :  Object.create
+
+//Object literals
+const mySym = Symbol('key')
+const obj = {
+    name : 'Priyanka',
+    age: 23,
+    [mySym] : 'myKey',
+    arr : ['hii', 'bye', 'good'],
+    bool : true,
+    "last name" : 'rout'  
+}
+// console.log(obj.name);
+// console.log(obj['name']);//another way to access object elements
+// console.log(obj['last name']);
+// console.log([mySym]);//to accesss Symbol use it inside [] without ''
+
+obj.name = 'Nisha'
+// console.log(obj);
+// Object.freeze(obj) //this would restrict any chnages made in the object after it is being written
+obj.name = 'Riya'
+// console.log(obj);
+
+obj.myFunc = function(){
+    console.log('Hello! i am xyz.');
+}
+obj.myFunc2 = function(){
+    console.log(`Hello! i am ${this.name}`);
+}
+// console.log(obj.myFunc());
+// console.log(obj.myFunc2());
+
+//const tinderUser = new Object()
+const tinderUser = {}// other way to declare object
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+const objUser = {
+    email : 'p@gmail.com',
+    age : 21,
+    userCred : {
+        UserName : {
+            firstName : 'Priyanka',
+            lastName : 'Rout'
+        }
+    }
+}
+// console.log(objUser.userCred.UserName.firstName);//we can access objects of objects 
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj3 = {5: "a", 6: "b"}
+
+const obj5 = {obj1, obj2}//it creates objects of object : { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+const obj6 = Object.assign({}, obj1, obj2, obj3)//copies all enumerable own properties from one or more source objects to a target object { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+const obj7 = {...obj1, ...obj2, ...obj3}//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const Objb = [      //array of objects
+    {
+        name : 'rahul',
+        age : 20
+    },{
+        name : 'rahul',
+        age : 20
+    },{
+        name : 'rahul',
+        age : 20
+    }
+]
