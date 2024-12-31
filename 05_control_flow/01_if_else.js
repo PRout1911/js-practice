87333852c2a15289ed07126e6c16c225058ca6b0
PRout1911 <routpriyanka1911@gmail.com>
2024-12-31 11:31:14 +0530

    If_else if_else

---
+++ 05_control_flow/01_if_else.js
@@ -0,0 +1,39 @@
+//if-else
+const temperature = 19
+if(temperature > 40){
+    console.log('Temperature is greater than 40');
+}else{
+    // console.log('Temperature is smaller than 40');
+}
+
+// (IMPORTANT)
+const score = 200
+if(score > 100){
+    var value = 'Hello' // VAR CREATES AMBIGUITY IN SCOPE AS IT TAKES EVERYTHING A GLOBAL SCOPE HENCE DO NOT USE VAR 
+}
+// console.log(`The user is saying ${value}`);
+
+//if-else if-else
+let num = 1000
+if(num < 200){
+    console.log('less than 200');
+}else if(num < 500){
+    console.log('less than 500');  
+}else if(num < 800){
+    console.log('less than 800');  
+}else{
+    // console.log('less than 1200');
+}
+
+//&& (and) , || (or)
+const loggedIn = true
+const debitCard = true
+const loggedInGoogle = true
+const loggedInEmail = false
+
+if(loggedIn && debitCard){//checks whether both the condition is true or not...
+    console.log('User can do shopping!');
+}
+if(loggedInEmail || loggedInGoogle){//checks whether either of the condition is true or not...
+    console.log('logged in using google or email');
+}
