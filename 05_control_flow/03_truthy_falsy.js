2024e85820575520cabcae1311cd43da9276818a
PRout1911 <routpriyanka1911@gmail.com>
2025-01-03 16:45:11 +0530

    truthy/falsy, nullish, terniary

--- 05_control_flow/03_truthy_falsy.js
+++ 05_control_flow/03_truthy_falsy.js
@@ -7,3 +7,36 @@ if (user) {
 }else{
     console.log('false');
 }
+
+//to check if array is empty
+const arr = []
+if(arr.length === 0){
+    console.log('Array is empty');
+}
+
+//to check if object is  empty
+const obj = {}
+if(Object.keys(obj).length === 0){
+    console.log('Object is empty');
+}
+
+//****
+false == 0 //true
+false == "" //true
+"" == 0 //true
+
+//Nullish Coalescing Operator (??): null undefined 
+// It is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, 
+// and otherwise returns its left-hand side operand.
+let val1 
+val1 = null ?? 10// 10
+val1 = 5 ?? 10//5
+val1 = undefined ?? 15 //15
+val1 = null ?? 10 ?? 20 //10 : first number will be choosen
+val1 = null ?? undefined //undefined : since undifined means there's something that exists
+console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80") //more than 80
