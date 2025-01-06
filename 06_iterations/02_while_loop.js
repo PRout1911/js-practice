60388692205226d184e4f8a9265e05b8184a4328
PRout1911 <routpriyanka1911@gmail.com>
2025-01-06 16:17:54 +0530

    while & do-while loop

---
+++ 06_iterations/02_while_loop.js
@@ -0,0 +1,21 @@
+//while
+let i = 0
+while (i <= 15) {
+    console.log(`Value of i: ${i}`);
+    i+=2
+}
+
+let myArr = ['Maths', 'Science', 'Hindi', 'English']
+let arr = 0
+while (arr <= myArr.length) {
+    console.log(`Value if ${arr} in array is: ${myArr[arr]}`);
+    arr++
+}
+
+//do - while
+
+let index = 1
+do {
+    console.log(`Value is: ${index}`);
+    index++
+} while (index <= 10);
