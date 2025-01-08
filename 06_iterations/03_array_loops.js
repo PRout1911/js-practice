4c9382994ec403c4fd788eb4da5ce23cc256f2c0
PRout1911 <routpriyanka1911@gmail.com>
2025-01-08 18:13:54 +0530

    array loops

---
+++ 06_iterations/03_array_loops.js
@@ -0,0 +1,11 @@
+//for - of
+const element = [1, 2, 3, 4, 5]
+for (const x of element) {
+    // console.log(x);
+}
+
+const string = "Priyanka Rout"
+for (const i of string) {
+    console.log(`Values in string is: ${i}`);
+}
+
