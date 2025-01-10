91d88c1b05551357e7f3d1815076f070359a16ec
PRout1911 <routpriyanka1911@gmail.com>
2025-01-10 12:17:25 +0530

    for-of loop

--- 06_iterations/03_for_of_loop.js
+++ 06_iterations/03_for_of_loop.js
@@ -9,12 +9,24 @@ for (const i of string) {
     console.log(`Values in string is: ${i}`);
 }
 
+//map
 const map = new Map()//maps are iterable, contains unique (key, value) pair
 map.set('IND', 'India')
 map.set('USA', 'America')
 map.set('FRA', 'France')
+for (const k of map) {
+    console.log(k);// prints ['key', 'value']
+}
 for (const [key, values] of map) {
-    console.log(key, '=>', values)
+    console.log(key, '=>', values)//prints key and values separately
 }
 
-//objects are not iterable using for-of loop
+//objects
+const myObject = {
+    game1: 'pubg',
+    game2: 'valorant'
+}
+for (const [key, value] of myObject) {
+    console.log(key, ':-', value);// ERROR : myObject is not iterable
+}
+//Hence, objects are not iterable using for-of loop
