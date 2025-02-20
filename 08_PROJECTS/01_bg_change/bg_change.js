9bdb0dd291d5be4a83f62d2549cb5ec26fdb8aa0
PRout1911 <routpriyanka1911@gmail.com>
2025-02-20 17:20:06 +0530

    project 1 - Bg color changer

--- 08_PROJECTS/01_bg_change/bg_change.js
+++ 08_PROJECTS/01_bg_change/bg_change.js
@@ -8,22 +8,31 @@ buttons.forEach(function (button) {
   button.addEventListener('click', function (e) {
     console.log(e.target);
 
-    // switch () {
-    //     case value:
-            
-    //         break;
-    
-    //     default:
-    //         break;
-    // }
-    // if (e.target.id === 'grey') {
-    //   body.style.backgroundColor = 'grey';
-    // } else if (e.target.id === 'yellow') {
-    //   body.style.backgroundColor = 'yellow';
-    // } else if (e.target.id === 'blue') {
-    //   body.style.backgroundColor = 'blue';
-    // } else if (e.target.id === 'purple') {
-    //   body.style.backgroundColor = 'purple';
-    // }
+    //using switch case
+    switch (e.target.id) {
+        case 'grey':
+          body.style.backgroundColor = 'grey';
+            break;
+        case 'yellow':
+          body.style.backgroundColor = 'yellow';
+            break;
+        case 'blue':
+          body.style.backgroundColor = 'blue';
+            break;
+        case 'purple':
+          body.style.backgroundColor = 'purple';
+            break;
+    }
+
+    //using if else
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = 'grey';
+    } else if (e.target.id === 'yellow') {
+      body.style.backgroundColor = 'yellow';
+    } else if (e.target.id === 'blue') {
+      body.style.backgroundColor = 'blue';
+    } else if (e.target.id === 'purple') {
+      body.style.backgroundColor = 'purple';
+    }
   });
 });
