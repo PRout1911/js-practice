938dc9d780210b34ed1e573b72792313c7af6c9b
PRout1911 <routpriyanka1911@gmail.com>
2025-02-23 21:15:46 +0530

    project - 4 : Guess the Number

---
+++ 08_PROJECTS/04_guess_number/guess_number.js
@@ -0,0 +1,24 @@
+const randNumber = parseInt(Math.random() * 100 + 1)
+
+const input = document.querySelector('#guessField')
+const submit = document.querySelector('#subt')
+const guesses = document.querySelector('.guesses')
+const lastResult = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const againStart = document.querySelector('.resultParas')
+
+let p = document.createElement('p')
+
+let prevGuess = []
+let numOfGuess = 1
+
+let userCanPlayGame = true
+if(userCanPlayGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+        const guess = parseInt(input.value)
+        validateTheGuess(guess);
+    });
+}
+
+function validateTheGuess(guessNum){}
