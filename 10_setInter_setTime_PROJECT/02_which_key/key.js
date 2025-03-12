3d71441182497a31deb224ca0c1d1980f63ddad7
PRout1911 <routpriyanka1911@gmail.com>
2025-03-12 22:10:30 +0530

    which key pressed

--- 10_setInter_setTime_PROJECT/02_which_key/key.js
+++ 10_setInter_setTime_PROJECT/02_which_key/key.js
@@ -11,8 +11,7 @@ window.addEventListener('keydown', function(e){
       <td>${e.key === ' ' ? 'Space' : e.key}</td>
       <td>${e.keyCode}</td> 
       <td>${e.code}</td>
-    </tr>
-    
+    </tr> 
   </table>
     </div>
   `;
