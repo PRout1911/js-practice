205d29124d0033201660e18a4cdd250b4ad6f03c
PRout1911 <routpriyanka1911@gmail.com>
2025-03-19 18:45:33 +0530

    promises

--- 11_API_&_promise_advance/promises.js
+++ 11_API_&_promise_advance/promises.js
@@ -1,4 +1,8 @@
-/*A Promise is in one of these states:
+/*
+Promise is an object which act as a placeholder for a value that will be available in the future.
+In simple words, promise is used to delay the activity or make it wait until the assigned tasks is completed.
+
+A Promise is in one of these states:
     pending: initial state, neither fulfilled nor rejected.
     fulfilled: meaning that the operation was completed successfully.
     rejected: meaning that the operation failed.
